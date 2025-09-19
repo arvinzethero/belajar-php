ba687edf0e8a5f1969d9e49d164d767ace225a25
arvinzethero <arvinhidayat12@gmail.com>
2025-09-19 20:04:40 +0700

    First Commit

---
+++ latihan1.php
@@ -0,0 +1,21 @@
+<?php
+// Pengulangan
+// for
+// while
+// do..while
+// foreach
+
+// for( $i = 0; $i < 5; $i++ ) {
+//     echo "<h1>Hello Bitch</h1>";
+// }
+// $i = 0;
+// while( $i < 5 ) {
+//     echo "Hello World <br>";
+//     $i++;
+// }
+$i = 0;
+do {
+   echo "Hello Wordl <br>";
+$i++; 
+} while($i < 5)
+?>
---
+++ latihan3.php
@@ -0,0 +1,14 @@
+<?php
+// Pengkondisian
+// if
+// else
+// else if
+// switch
+
+$x = 10;
+if( $x < 20 ) {
+    echo "Benar";
+} else {
+    echo "Salah";
+}
+?>
