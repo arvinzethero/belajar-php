7ec88b9176707f29ca957e89b5d00edd4fb2d185
arvinzethero <arvinhidayat12@gmail.com>
2025-09-21 18:31:45 +0700

    mantap jiwa

--- latihan4.php
+++ latihan4.php
@@ -5,6 +5,14 @@
     // echo date("l, d-M-Y");
 
 // Time
-echo time();
-    
+// echo time();
+// echo date("l", time() - 60 * 60 * 24 * 1);
+// mktime
+// membuat sendiri detik
+// mktime(0,0,0,0,0,0)
+// jam, menit, detik, bulan, tanggal, tahun
+// echo date("l",mktime(0,0,0,10,11,1995)); 
+
+// strtotime
+echo date("l",strtotime("10 nov 1998"));
  ?>
